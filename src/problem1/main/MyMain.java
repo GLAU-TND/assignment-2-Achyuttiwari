a91bbeb76644b3af3542492bc7bd546361986ddd
AchyutTiwari <achyut.tiwari_cs8@gla.ac.in>
2020-03-28 00:25:36 +0530

    making the variables private

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -1,8 +1,8 @@
 /*
  *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:17 PM
+ *  User: Achyuttiwari
+ *  Date: 27-Mar-20
+ *  Time: :17 PM
  */
 package problem1.main;
 // executable class

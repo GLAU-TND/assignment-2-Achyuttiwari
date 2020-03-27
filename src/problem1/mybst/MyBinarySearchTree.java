b51394fa25c17e241fb03aff1edb253b5cf16b21
AchyutTiwari <achyut.tiwari_cs8@gla.ac.in>
2020-03-28 00:45:07 +0530

    taken two private variable Arraylist which is of integer type and the other one is root which is of treenode type

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -5,7 +5,15 @@
  *  Time: 8:59 PM
  */
 package problem1.mybst;
+import java.util.ArrayList;
+import problem1.node.TreeNode;
+
 // to implement BinarySearchTree
 public class MyBinarySearchTree {
+
+    private TreeNode root;
+    private ArrayList<Integer> arrayList;
+
+
 
 }

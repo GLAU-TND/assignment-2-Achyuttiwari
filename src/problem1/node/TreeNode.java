15b18321da36da67980ee664a208038ba8a4292e
AchyutTiwari <achyut.tiwari_cs8@gla.ac.in>
2020-03-28 00:34:32 +0530

    using the getter and setter for data to reieve the data privately without using

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -1,10 +1,20 @@
 /*
  *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:17 PM
+ *  User: Achyuttiwari
+ *  Date: 27-Mar-20
+ *  Time: 8:52 PM
  */
 package problem1.node;
 
 public class TreeNode {
+    private int data;
+    private TreeNode leftNode;
+    private TreeNode rightNode;
+
+    public int getData() {
+        return data;
+    }
+    public void setData(int data) {
+        this.data = data;
+    }
 }

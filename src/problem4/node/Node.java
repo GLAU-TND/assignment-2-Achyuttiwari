85caa354b476460d1aabf97055775c379711aaeb
AchyutTiwari <achyut.tiwari_cs8@gla.ac.in>
2020-03-28 15:16:52 +0530

    getter and setter of data to fetch the value

--- src/problem4/node/Node.java
+++ src/problem4/node/Node.java
@@ -3,4 +3,13 @@ package problem4.node;
 public class Node {
     private int data;
     private Node next;
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
 }

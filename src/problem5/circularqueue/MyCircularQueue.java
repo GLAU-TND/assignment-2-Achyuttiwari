4386b7956db01dcdc2510536b06d8c368df1ffe6
AchyutTiwari <achyut.tiwari_cs8@gla.ac.in>
2020-03-28 16:21:29 +0530

    getter and setter for front and rear

--- src/problem5/circularqueue/MyCircularQueue.java
+++ src/problem5/circularqueue/MyCircularQueue.java
@@ -11,4 +11,18 @@ import problem5.node.Node;
 public class MyCircularQueue {
     private Node front;
     private Node rear;
+    public Node getFront() {
+        return front;
+    }
+
+    public void setFront(Node front) {
+        this.front = front;
+    }
+    public Node getRear() {
+        return rear;
+    }
+
+    public void setRear(Node rear) {
+        this.rear = rear;
+    }
 }
